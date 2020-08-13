742b99ecd2950db0aaf60a133732fb6afe3f5892
Rashad <mkrashad25@gmail.com>
2020-08-13 16:22:25 +0200

    Added linked_list.js file

---
+++ data_structures/stack(methods).py
@@ -0,0 +1,11 @@
+my_stack = list()
+my_stack.append(4)
+my_stack.append(7)
+my_stack.append(12)
+my_stack.append(19)
+print(my_stack)
+
+print(my_stack.pop())
+print(my_stack.pop())
+print(my_stack)
+
